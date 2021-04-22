9a5087b2abeb33d0a447167f539da343ebb12e99
Shishir Thapa <shishirthapa1@gmail.com>
2021-04-22 17:12:29 +0545

    post crud

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+
+    has_many :posts
+
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true
+
+    def full_name
+        first_name + ' ' + last_name
+    end
 end
