f9e2da505b622622e4a1c6eba343b77c7b72fd35
Shishir Thapa <shishirthapa1@gmail.com>
2021-04-22 16:49:53 +0545

    update/edit/destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,42 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.valid?
+            user.save
+        else
+            puts "User data not good"
+        end
+        redirect_to users_path
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+
+        redirect_to user_path(user)
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+
+        redirect_to users_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email)
+    end
 end
